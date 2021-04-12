90e0c0529c104fcb075f9bed007034e53765e860
Sabrina <sabrinapea@yahoo.com>
2021-04-12 14:25:49 -0700

    step 6 exercises

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,15 +1,37 @@
 //STEP 1
-let favMovies = ['Inception', ' Life of Walter Mitty', 'Lord of the Rings', 'Harry Potter']
+// let movies = ['Inception', ' Life of Walter Mitty', 'Lord of the Rings', 'GOT', 'Forest Gump'];
+//   console.log(movies[2]);
 
 //STEP 2
+// let movies = new Array(5)
+// movies[0] = 'Inception';
+// movies[1] = 'Life Of Walter Mitty';
+// movies[2] = 'Lord of the Rings';
+// movies[3] = 'Games of Thrones';
+// movies[4] = 'Forest Gump';
+// console.log(movies[0]);
 
 //STEP 3
+// let movies = new Array('Inception', 'Life Of Walter Mitty', 'Lord of the Rings', 'Games of Thrones', 'Forest Gump');
+// movies.splice(movies.length / 2, 0, 'Harry Potter');
+// console.log(movies);
 
 //STEP 4
+// let movies = ['Inception', ' Life of Walter Mitty', 'Lord of the Rings', 'GOT', 'Forest Gump'];
+// delete movies[0];
+// console.log(movies);
 
 //STEP 5
+let movies = ['Inception', 'Life of Walter Mitty', 'Lord of the Rings', 'GOT', 'Forest Gump', 'Harry Potter', 'LaLa Land'];
+for (let moviesNames in movies) {
+    console.log(movies);
+}
 
 //STEP 6
+// let movies = ['Inception', 'Life of Walter Mitty', 'Lord of the Rings', 'GOT', 'Forest Gump', 'Harry Potter', 'LaLa Land'];
+// for (let i of movies) {
+//     console.log(i);
+// }
 
 //STEP 7
 
