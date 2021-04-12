937eabda6a1e1bca87411f63601127ecce922331
Sabrina <sabrinapea@yahoo.com>
2021-04-12 10:54:58 -0700

    init assignment08

---
+++ js/01-arrays-practice.js
@@ -0,0 +1,34 @@
+//STEP 1
+let favMovies = ['Inception', ' Life of Walter Mitty', 'Lord of the Rings', 'Harry Potter']
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
